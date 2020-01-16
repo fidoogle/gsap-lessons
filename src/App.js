b5b0bfe4b9cc6ad74563b684ac4c5fcc5436c9ab
Fidel Guajardo <figuajardo@saws.org>
2020-01-16 09:10:08 -0600

    add titles for MUI vs Flexbox

--- src/App.js
+++ src/App.js
@@ -9,8 +9,10 @@ function App() {
 
   return (
     <div className="App">
+      <h2>These are all Material UI based.</h2>
       <Tile/>
       <br/><br/><br/>
+      <h2>These are all Flexbox based with sprinkles of Material UI</h2>
       <TileFlex/>
     </div>
   );
